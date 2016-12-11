81d683d3f25f66058872830475ca9920e9906413
Adnan Begovic <adnan@cyngn.com>
2016-12-10 23:32:35 -0500

    SDK: Declare a parcelable header
    Change-Id: Id9c4abbd15d64cb0603e11a00187e19ec83187d7

--- src/java/aoscp/os/Build.java
+++ src/java/aoscp/os/Build.java
@@ -27,6 +27,16 @@ public class Build {
     /** Value used for when a build property is unknown. */
     public static final String UNKNOWN = "unknown";
 
+	/**
+     * Since there might be a case where new versions of the framework use applications running
+     * old versions of the protocol (and thus old versions of this class), we need a versioning
+     * system for the parcels sent between the core framework and its sdk users.
+     *
+     * This parcelable version should be the latest version API version listed in
+     * {@link AOSCP_VERSION_API}
+     */
+    public static final int PARCELABLE_VERSION = AOSCP_VERSION_API.CHEESECAKE;
+	
     private static final SparseArray<String> sdkMap;
     static
     {
